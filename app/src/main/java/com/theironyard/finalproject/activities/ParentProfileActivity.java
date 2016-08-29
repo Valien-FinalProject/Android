a439c266a32547e699fdf81ccec7e5caf2cc03c4
vasantia <vasantia@gmail.com>
2016-08-28 21:47:22 -0700

    layout changes

--- app/src/main/java/com/theironyard/finalproject/activities/ParentProfileActivity.java
+++ app/src/main/java/com/theironyard/finalproject/activities/ParentProfileActivity.java
@@ -3,8 +3,6 @@ package com.theironyard.finalproject.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -248,7 +246,7 @@ public class ParentProfileActivity extends AppCompatActivity
                                     android.R.layout.simple_list_item_1,
                                     choreNames);
                     ListView myList=(ListView)
-                            findViewById(R.id.pProfileChoresPendingListView);
+                            findViewById(R.id.pProfileChoresCompletedListView);
                     myList.setAdapter(stringArrayAdapter);
                 }
 
